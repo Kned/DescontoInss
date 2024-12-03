f4620e6bcac8b273e3ba22286e32396f09bae1e6
Eduardo Kettz Nunes <kettz_0@hotmail.com>
2024-12-03 08:34:49 -0300

    fixing rounding issue, removing unused model spec

--- app/services/calcula_desconto_service.rb
+++ app/services/calcula_desconto_service.rb
@@ -1,18 +1,20 @@
 class CalculaDescontoService
   def self.calcula_desconto salario
+    raise StandardError.new "Salario type invalid" unless salario.is_a? Numeric
     faixas = [
-    {min: 0.0, max: 1045.0, tax: 7.5},
-    {min: 1045.1, max: 2089.60, tax: 9.0},
-    {min: 2089.61, max: 3134.40, tax: 12.0},
-    {min: 3134.41, max: 6101.06, tax: 14.0}
+    {min: 0.0, max: 1045.0, tax: 0.075},
+    {min: 1045.1, max: 2089.60, tax: 0.09},
+    {min: 2089.61, max: 3134.40, tax: 0.12},
+    {min: 3134.41, max: 6101.06, tax: 0.14}
   ]
 
   total_recolher = 0
+  
   faixas.each do |faixa|
     max = faixa[:max] <= salario ? faixa[:max] : salario
-    total_recolher += (max - faixa[:min]) * (faixa[:tax]/100)
+    total_recolher += (max - faixa[:min]) * faixa[:tax]
     break if max == salario
   end
-  total_recolher.round(2)
+  total_recolher.floor(2)
   end
 end
--- spec/services/calcula_desconto_service_spec.rb
+++ spec/services/calcula_desconto_service_spec.rb
@@ -4,16 +4,36 @@ require 'rails_helper'
 
 RSpec.describe CalculaDescontoService, type: :service do
   describe 'calcula_desconto' do
+    context 'number is valid' do
       it 'calcula desconto corretamente' do
         expect(
           described_class.calcula_desconto(3000.00)
-        ).to eq(281.63)
+        ).to eq(281.62)
         expect(
           described_class.calcula_desconto(932.50)
-        ).to eq(69.94)
+        ).to eq(69.93)
         expect(
           described_class.calcula_desconto(1204.00)
-        ).to eq(92.68)
+        ).to eq(92.67)
       end
+    end
+
+    context 'number is invalid' do
+      it 'generates error' do
+        expect{
+          described_class.calcula_desconto("invalid")
+        }.to raise_error(StandardError)
+      end
+    end
+    context 'number is a integer' do
+      it 'calcula desconto corretamente' do
+        expect(
+          described_class.calcula_desconto(3000)
+        ).to eq(281.62)
+        expect(
+          described_class.calcula_desconto(1204)
+        ).to eq(92.67)
+      end
+    end
   end
 end
